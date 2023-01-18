444d90e7addea8833a2cbb0960a5835bd1e6faca
officialpradip <study.pradipthapa@gmail.com>
2023-01-18 08:55:52 +0545

    locators

--- Selenium Practice/first.py
+++ Selenium Practice/first.py
@@ -11,11 +11,27 @@
 from selenium import webdriver
 import time
 # driver is object of Chrome() class
-driver = webdriver.Chrome(
-    executable_path="F:\Automation Testing\selenium\chromedriver_win32\chromedriver.exe")  # chrome() class
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+#selenium 4
+service_obj=Service("F:\Automation Testing\selenium\chromedriver_win32\chromedriver.exe")
+driver = webdriver.Chrome(service=service_obj)  # chrome() class
+
 # chrome drive path: "F:\Automation Testing\selenium\chromedriver_win32\chromedriver.exe"
 driver.get('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
 
-driver.find_element_by_name("username").send_keys("Admin")
+#driver.maximize_window() # For maximizing window
+driver.implicitly_wait(20) # gives an implicit wait for 20 seconds
 
-#time.sleep(1000)
+driver.find_element(By.NAME,"username").send_keys("Admin")
+driver.find_element(By.NAME,"password").send_keys("admin123")
+driver.find_element(By.CSS_SELECTOR,"button[type='submit']").click()
+
+time.sleep(1000)
+
+actualTitle=driver.title
+expectedTitle ="OrangeHRM"
+if actualTitle==expectedTitle:
+    print("Test Passed")
+else:
+    print("Test Failed")
