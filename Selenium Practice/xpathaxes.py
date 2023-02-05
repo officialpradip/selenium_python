da0a8fe76dd16cce60bc7c6f5788cdd2384bad66
officialpradip <study.pradipthapa@gmail.com>
2023-02-05 15:55:17 +0545

    self xpathaxes

---
+++ Selenium Practice/xpathaxes.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+service_object = Service(
+    "F:\Automation Testing\selenium\chromedriver_win32\chromedriver.exe")
+demo = webdriver.Chrome(service=service_object)
+
+demo.get("https://money.rediff.com/gainers/bse/daily/groupall")
+
+# self
+details = demo.find_element(
+    By.XPATH, "//a[normalize-space()='Prime Fresh']/self::a").text
+print("++++++++", details)
