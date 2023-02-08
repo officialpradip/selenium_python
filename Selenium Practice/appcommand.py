59689f05d38625e325a825173be3ccaca983d62b
officialpradip <study.pradipthapa@gmail.com>
2023-02-08 11:47:18 +0545

    application command

---
+++ Selenium Practice/appcommand.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+obj_serv = Service(
+    "F:\Automation Testing\selenium\chromedriver_win32\chromedriver.exe")
+demo = webdriver.Chrome(service=obj_serv)
+
+demo.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+# application command
+print("Title of Site is:", demo.title)
+print("Url of Site is:", demo.current_url)
+print("Page Source of site is:", demo.page_source)
