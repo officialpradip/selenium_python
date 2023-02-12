7bb0f5ef09ae3d20a4444defc591d4f049d2d43e
officialpradip <study.pradipthapa@gmail.com>
2023-02-12 09:55:31 +0545

    explicit

--- Selenium Practice/explicitwait.py
+++ Selenium Practice/explicitwait.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 obj_serv = Service(
     "F:\Automation Testing\selenium\chromedriver_win32\chromedriver.exe")
@@ -14,7 +15,7 @@ searchbox = demo.find_element(By.NAME, "q")
 searchbox.send_keys("selenium")
 searchbox.submit()
 
+search = mywait.until(EC.presence_of_element_located(
+    By.XPATH, '//div[text()="Selenium"]'))
 
-search = demo.find_element(
-    By.XPATH, '//div[text()="Selenium"]')
 search.click()
