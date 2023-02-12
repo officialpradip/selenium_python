7ece3dee0b03a0d9d78427f2fc01d346bcff75b4
officialpradip <study.pradipthapa@gmail.com>
2023-02-12 15:45:36 +0545

    selecting checkbox

--- Selenium Practice/box.py
+++ Selenium Practice/box.py
@@ -2,8 +2,20 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
-service_obj=Service('F:\Automation Testing\selenium\chromedriver_win32\chromedriver.exe')
-demo=webdriver.Chrome(service=service_obj)
+service_obj = Service(
+    'F:\Automation Testing\selenium\chromedriver_win32\chromedriver.exe')
+demo = webdriver.Chrome(service=service_obj)
 
-#get website
+# get website
 demo.get("https://itera-qa.azurewebsites.net/home/automation")
+
+# select single checkbox
+# monday = demo.find_element(
+#     By.XPATH, "//label[normalize-space()='Monday']").click()
+
+# selecting all elements
+all = demo.find_elements(
+    By.XPATH, "//input[@type='checkbox' and contains(@id,'day')]")
+print(len(all))
+for i in range(len(all)):
+    all[i].click()
