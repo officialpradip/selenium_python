07b0160ec35fab291f4c49137988840361858498
officialpradip <study.pradipthapa@gmail.com>
2023-02-13 13:14:56 +0545

    fix automatically closing

--- Selenium Practice/links.py
+++ Selenium Practice/links.py
@@ -4,12 +4,11 @@ from selenium.webdriver.common.by import By
 import time
 servic_obj = Service(
     "F:\Automation Testing\selenium\chromedriver.exe")
-demo = webdriver.Chrome(service=servic_obj)
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+demo = webdriver.Chrome(service=servic_obj, options=options)
+
 
 demo.get("https://demo.nopcommerce.com/")
 demo.maximize_window()
-
-trys = demo.find_element(By.LINK_TEXT, "Digital downloads ")
-trys.click()
-time.sleep(10)
-demo.close()
