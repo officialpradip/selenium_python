332ffae7d4321292823749b87e2aa019bb48f98c
officialpradip <study.pradipthapa@gmail.com>
2023-02-13 09:09:08 +0545

    change driver

--- Selenium Practice/links.py
+++ Selenium Practice/links.py
@@ -1,9 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-
+import time
 servic_obj = Service(
-    "F:\Automation Testing\selenium\chromedriver_win32\chromedriver.exe")
+    "F:\Automation Testing\selenium\chromedriver.exe")
 demo = webdriver.Chrome(service=servic_obj)
 
 demo.get("https://demo.nopcommerce.com/")
+demo.maximize_window()
+
+trys = demo.find_element(By.LINK_TEXT, "Digital downloads ")
+trys.click()
+time.sleep(10)
+demo.close()
