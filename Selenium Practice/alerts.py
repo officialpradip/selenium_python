fc35a0796a74244f71bb01aea41d32c7bfa2a860
officialpradip <study.pradipthapa@gmail.com>
2023-02-14 09:10:47 +0545

    alert setup

---
+++ Selenium Practice/alerts.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+import time
+servic_obj = Service(
+    "F:\Automation Testing\selenium\chromedriver.exe")
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+
+demo = webdriver.Chrome(service=servic_obj, options=options)
+demo.get("https://the-internet.herokuapp.com/javascript_alerts")
+# open alert windows
+demo.find_element(
+    By.XPATH, "(//button[normalize-space()='Click for JS Prompt'])").click()
+
+# need to switch as alert is not driver elements
+alertWindows = demo.switch_to.alert
+print(alertWindows.text)
+alertWindows.send_keys("Hello")
+alertWindows.accept()  # close alert
