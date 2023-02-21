d8ea7ec85193e42c9e5eb88f345c5ce26bbb6733
officialpradip <study.pradipthapa@gmail.com>
2023-02-21 12:57:10 +0545

    auth alert setup

--- Selenium Practice/authalerts.py
+++ Selenium Practice/authalerts.py
@@ -1,1 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+ser_obj = Service("F:\Automation Testing\selenium\chromedriver.exe")
+demo = webdriver.Chrome(service=ser_obj)
+website = demo.get("https://the-internet.herokuapp.com/basic_auth")
