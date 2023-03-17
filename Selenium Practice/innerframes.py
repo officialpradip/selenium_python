05f1db102d6bdf3d08ecdb739ba3af411cf3b7ac
officialpradip <study.pradipthapa@gmail.com>
2023-03-17 13:09:55 +0545

    iframes

--- Selenium Practice/innerframes.py
+++ Selenium Practice/innerframes.py
@@ -5,9 +5,15 @@ import time
 serv_obj = Service("F:\Automation Testing\selenium\chromedriver.exe")
 demo = webdriver.Chrome(service=serv_obj)
 page = demo.get('https://demo.automationtesting.in/Frames.html')
-iframes = demo.find_element(
-    By.XPATH, "//a[normalize-space()='Iframe with in an Iframe']")
+# iframes = demo.find_element(
+#     By.XPATH, "//a[normalize-space()='Iframe with in an Iframe']")
 
-iframes.click()
+# iframes.click()
 
+# outerframes = demo.find_element(By.XPATH, '//*[@id="Multiple"]/iframe')
+# demo.switch_to.frame(outerframes)
+
+innerframes = demo.find_element(By.XPATH, "//iframe[@id='singleframe']")
+demo.switch_to.frame(innerframes)
+demo.find_element(By.CSS_SELECTOR, "input[type='text']").send_keys("DEMO")
 time.sleep(100)
