b1ed313b3498567ff691fb9e4677162bf4d424f9
officialpradip <study.pradipthapa@gmail.com>
2023-03-17 13:02:05 +0545

    iframes setup

--- Selenium Practice/innerframes.py
+++ Selenium Practice/innerframes.py
@@ -1,7 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
-
+import time
 serv_obj = Service("F:\Automation Testing\selenium\chromedriver.exe")
 demo = webdriver.Chrome(service=serv_obj)
-page = demo.get('https://www.selenium.dev/documentation/overview/')
+page = demo.get('https://demo.automationtesting.in/Frames.html')
+iframes = demo.find_element(
+    By.XPATH, "//a[normalize-space()='Iframe with in an Iframe']")
+
+iframes.click()
+
+time.sleep(100)
